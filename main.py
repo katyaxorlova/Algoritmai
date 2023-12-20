c58941570bd9bc3d46500132ecf0ea998a2f07e7
katyaaa <72643717+katyaxorlova@users.noreply.github.com>
2023-12-20 10:54:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,57 @@
-answer = float('inf')
-min_path = []
+import numpy as np
+import networkx as nx
+from itertools import permutations
 
-
-def tsp(graph, v, currPos, n, count, cost, path):
-
-    global answer, min_path
-    if count == n and graph[currPos][0]:
-        if cost + graph[currPos][0] < answer:
-            answer = cost + graph[currPos][0]
-            min_path = path.copy()
-
-    for i in range(n):
-        if not v[i] and graph[currPos][i]:
-            
-            # pažymim kaio aplankyta
-            v[i] = True
-            tsp(graph, v, i, n, count + 1, cost + graph[currPos][i], path + [i])
-            
-            # pažymim kaip neaplankyta
-            v[i] = False
-
-# skaitom duomenis iš file
-def read_graph_from_file(file_name):
+def read_matrix_from_file(file_name):
     with open(file_name, 'r') as file:
         lines = file.readlines()
-        n = len(lines)
-        graph = [[int(val) for val in line.split()] for line in lines]
-        return graph, n
+        matrix = [list(map(int, line.strip().split())) for line in lines]
+    return np.array(matrix)
 
+def calculate_cost(path, graph):
+    cost = 0
+    for i in range(len(path) - 1):
+        cost += graph[path[i]][path[i + 1]]['weight']
+    return cost
 
-if __name__ == '__main__':
-    file_name = "grafas.txt"
-    graph, n = read_graph_from_file(file_name)
+def traveling_salesman(graph):
+    num_nodes = len(graph.nodes)
+    all_nodes = set(graph.nodes)
+    start_node = 1  # Pradedame nuo mazgo 1
 
-    #patikra ar miestas buvo aplankytas
-    v = [False for _ in range(n)]
+    best_path = None
+    best_cost = float('inf')
+
+    # Sugeneruojame visas galimas keliones, kurios prasideda ir baigiasi pirmame mieste
+    all_paths = permutations(all_nodes - {start_node})
+    for path in all_paths:
+        current_path = (start_node,) + path + (start_node,)
+        current_cost = calculate_cost(current_path, graph)
+
+        # Atnaujiname geriausią kelią ir sanaudas, jei randame optimalesni varianta
+        if current_cost < best_cost:
+            best_path = current_path
+            best_cost = current_cost
+
+    return best_path, best_cost
+
+def main():
+    file_name = 'grafas.txt'  
+    adjacency_matrix = read_matrix_from_file(file_name)
+
+    # Grafo sukurimas naudojant networkx
+    graph = nx.Graph()
+    num_nodes = len(adjacency_matrix)
+
+    for i in range(num_nodes):
+        for j in range(i + 1, num_nodes):
+            graph.add_edge(i + 1, j + 1, weight=adjacency_matrix[i][j])
     
-    # primą miestą pažymim aplankytu
-    v[0] = True
+    # Surandame optimalų kelią ir sąnaudas
+    optimal_path, optimal_cost = traveling_salesman(graph)
 
-    # minimalios kainos paieška
-    tsp(graph, v, 0, n, 1, 0, [0])
+    print("Optimalus Kelias:", optimal_path)
+    print("Optimalios Sąnaudos:", optimal_cost)
 
-    # Cpatikra ar galime iš paskutinio miesto sugrįžti į pradinį
-    if min_path and min_path[-1] != min_path[0]:
-        min_path.append(min_path[0])  
-
-    # spausdinimas
-    print("Kaina:", answer)
-    print("Kelias:", min_path)
+if __name__ == "__main__":
+    main()
